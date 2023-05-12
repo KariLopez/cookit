531878f2c8f72b567968ff89caab30fdaea8d7c6
KariLopez <karinalopez91@gmail.com>
2023-05-11 20:45:34 -0400

    Initial Commit

---
+++ cookit/cookitApp.swift
@@ -0,0 +1,17 @@
+//
+//  cookitApp.swift
+//  cookit
+//
+//  Created by Karina Lopez on 5/11/23.
+//
+
+import SwiftUI
+
+@main
+struct cookitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
